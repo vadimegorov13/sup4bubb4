c857a58346a8a9e82c88f50edf34f92c9c5e90b1
Vadim Egorov <vadegor00@gmail.com>
2022-08-09 13:25:14 -0800

    smaller footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <div class="flex items-center justify-between pt-10 mx-auto">
   <div class="flex items-center flex-shrink-0 mx-auto">
-    <span class="font-semibold text-md mx-auto">
+    <span class="font-semibold text-xs mx-auto">
       made by a <a
         href="https://twitter.com/vodem_e"
         class="hover:text-ameprimary"
@@ -11,14 +11,14 @@
     <img
       src="./amelialove.webp"
       alt="amelialove"
-      width="32"
-      height="32"
+      width="16"
+      height="16"
       style="margin-left: 5px"
     />
   </div>
 </div>
 <div class="flex items-center flex-shrink-0 mx-auto">
-  <span class="font-semibold text-md mx-auto">
+  <span class="font-semibold text-xs mx-auto">
     <a
       href="https://github.com/vadimegorov13/sup4bubb4"
       class="hover:text-ameprimary"
