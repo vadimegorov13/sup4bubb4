38558de877facf251e24ac0d90e44a85655d30d6
Vadim Egorov <vadegor00@gmail.com>
2022-08-09 10:00:21 -0800

    init firebase

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,11 @@
+/**
+ * Firebase configuration
+ */
+export const firebaseConfig = {
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECT_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_APP_ID,
+};
