b783d327d3f4a5dd4c812ead5144cdf4718c2ece
Vadim Egorov <vadegor00@gmail.com>
2022-09-10 15:17:15 -0800

    added web-vitals for analytics on vercel

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,36 @@
+<script context="module" lang="ts">
+  import type { Load } from '@sveltejs/kit';
+  export const load: Load = async ({ props: { params, path } }) => {
+    return {
+      props: {
+        params,
+        path,
+      },
+    };
+  };
+</script>
+
 <script lang="ts">
   import '../app.css';
+  import { onMount } from 'svelte';
+  import { page } from '$app/stores';
+  import { webVitals } from '$lib/webvitals';
+
+  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID as string;
+  export let path: string;
+  export let params: Record<string, string>;
+
+  page.subscribe((page) => {
+    path = page.url.href;
+    params = page.params;
+  });
+
+  console.log('path:', path);
+  console.log('params:', params);
+
+  onMount(() => {
+    if (analyticsId) webVitals({ path, params, analyticsId });
+  });
 </script>
 
 <div style="background-color: #181818" class="min-h-screen text-white">
--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,12 @@ const config = {
   preprocess: [
     preprocess({
       postcss: true,
+      replace: [
+        [
+          'import.meta.env.VERCEL_ANALYTICS_ID',
+          JSON.stringify(process.env.VERCEL_ANALYTICS_ID),
+        ],
+      ],
     }),
   ],
 
